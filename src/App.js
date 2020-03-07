e65d70e57422edd3828b6cad5a64910daf83e459
hatieudao <froyal2kk1@gmail.com>
2020-03-07 21:53:00 +0700

    Add first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import TodoItem from './components/TodoItem';
+class App extends Component {
+  constructor(){
+    super();
+    this.todoItems = [
+      {title:"Go Library", isComplete: true},
+      {title:"Study", isComplete: true},
+      {title:"Have Lunch"}
+    ]
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {
+       this.todoItems.map( (item,index)=>
+       <TodoItem key={index} item={item} />
+       )
+     }
     </div>
-  );
+  )
+}
 }
 
 export default App;
